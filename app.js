2132b078365763b78124e25c77b2c16ef73655d1
Maroko Gideon <marokogideon@gmail.com>
2023-04-28 11:23:02 +0300

    add home page

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const homeRouter = require("./routes/home");
 
 const app = express();
+
+app.set('view engine', "ejs");
+
+app.use(homeRouter.router);
+
+// process.env.DB_URL
 
 app.listen(5500, ()=>{
     console.log("Application is starting...");
