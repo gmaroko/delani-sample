b76f527cf3c9970f0e6cbff638dc171140e4ff72
Eliud Gateri <egateri@gmail.com>
2023-05-02 20:20:48 +0300

    Add login route

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const homeRouter = require("./routes/home");
-
+const loginRouter = require("./routes/login");
 const app = express();
 
 app.set('view engine', "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(homeRouter.router);
+app.use(loginRouter);
 
 // process.env.DB_URL
 
